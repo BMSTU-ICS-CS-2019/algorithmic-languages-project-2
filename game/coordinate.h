31b27c5c78ea0048515ff3befcb52750dec7c9ee
JarvisStark <mrjarviscraft@gmail.com>
2019-12-17 00:44:11 +0300

    Make Coordinate members non-const

--- game/coordinate.h
+++ game/coordinate.h
@@ -5,7 +5,7 @@
 namespace battleships {
 
     struct Coordinate {
-        const size_t x, y;
+        size_t x, y;
 
         inline Coordinate(const size_t &x, const size_t &y): x(x), y(y) {}
     };
